30d822e96577b2575a6e88f0e5cb173c8e814039
Kevin Kim <kevink1103@gmail.com>
2019-10-08 23:18:14 +0800

    first day

---
+++ python/temp.py
@@ -0,0 +1,15 @@
+class Comedian:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} is {self.age}."
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name} is {self.age}. Surprise!"
+
+new_comedian = Comedian("Eric", "Idle", "74")
+print(new_comedian)
+print(f"{new_comedian}")
